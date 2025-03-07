72b51f62a13117626407fdc9eec69bf7560546bf
bharishh <harish_01@icloud.com>
2025-03-07 16:33:28 +0100

    include

---
+++ includes/headerCo.inc.php
@@ -0,0 +1,14 @@
+<header>
+        <figure>
+          <a href="index.html"><img src="./asset/mail.png" alt="Gmail logo" title="Google Gmail"></a>
+          <figcaption hidden>Logo Gmail</figcaption>
+        </figure>
+        <h1><a href="index.html">Gmail</a></h1>
+        <nav>
+          <ul>
+            <li><a href="#">POUR LES PROS</a></li>
+            <li class="connect-acc">CONNEXION</a></li>
+            <li><a href="index.html#formu">CREER UN COMPTE</a></li>
+          </ul>
+        </nav>
+      </header>
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<?php
-include_once __DIR__."/includes/head.inc.php";
-?>
-
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Gmail</title>
+</head>
 <body>
 
 <?php
@@ -54,7 +58,7 @@ include_once __DIR__."/includes/header.inc.php";
         </section>  
    
 <?php
-    include_once __DIR__."/includes/footer.inc.php";
+    include_once __DIR__."/includes/footer.inc.php"
 ?>
 
     </main>
